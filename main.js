86aeed3c0263e7e350bc713a8ae66faec50482bb
DimeP1 <71714575+DimeP1@users.noreply.github.com>
2020-10-06 15:16:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const firstName = "Diamond"
+const lastName = "Patterson"
+const age = 21;
+
+alert ('My name is ' +firstName +lastName);
+alert ('I am ' +age +'years old');
+
+if (age > 21) {
+    alert ('I am legally able to drink in the states.');
+} else if(age = 21) {
+    alert ('I am just barely able to drink legally in the states.')
+}else {
+    alert ('It is illegal for me to drink in the states.')
+}
+
+console.log('I am starting to have a lot of fun in this class.')
